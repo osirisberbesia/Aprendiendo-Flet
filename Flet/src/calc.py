ac7956593a5fc0c65e12ad7c33397b9d8ba931e3
Osiris <berbesiag@gmail.com>
2026-03-06 12:07:13 -0400

    Calc

--- Flet/src/calc.py
+++ Flet/src/calc.py
@@ -1,46 +1,44 @@
 from dataclasses import field
-
 import flet as ft
-
 
 @ft.control
 class CalcButton(ft.Button):
     expand: int = field(default_factory=lambda: 1)
-
 
 @ft.control
 class DigitButton(CalcButton):
     bgcolor: ft.Colors = ft.Colors.WHITE_24
     color: ft.Colors = ft.Colors.WHITE
 
-
 @ft.control
 class ActionButton(CalcButton):
     bgcolor: ft.Colors = ft.Colors.ORANGE
     color: ft.Colors = ft.Colors.WHITE
-
 
 @ft.control
 class ExtraActionButton(CalcButton):
     bgcolor: ft.Colors = ft.Colors.BLUE_GREY_100
     color: ft.Colors = ft.Colors.BLACK
 
-
 @ft.control
 class CalculatorApp(ft.Container):
     def init(self):
-        self.reset()
+        self.expression = ""
         self.width = 350
         self.bgcolor = ft.Colors.BLACK
         self.border_radius = ft.BorderRadius.all(20)
         self.padding = 20
-        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
+        
+        self.formula = ft.Text(value="", color=ft.Colors.WHITE60, size=14)
+        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=35)
+        self.preview = ft.Text(value="", color=ft.Colors.GREY_500, size=18)
 
         self.content = ft.Column(
             controls=[
-                ft.Row(
-                    controls=[self.result],
+                ft.Column(
+                    controls=[self.formula, self.result, self.preview],
                     alignment=ft.MainAxisAlignment.END,
+                    horizontal_alignment=ft.CrossAxisAlignment.END,
                 ),
                 ft.Row(
                     controls=[
@@ -88,85 +86,98 @@ class CalculatorApp(ft.Container):
 
     def button_clicked(self, e):
         data = e.control.content
-        print(f"Button clicked with data = {data}")
-        if self.result.value == "Error" or data == "AC":
+        
+        if data == "AC":
+            self.expression = ""
             self.result.value = "0"
-            self.reset()
+            self.formula.value = ""
+            self.preview.value = ""
 
         elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
-            if self.result.value == "0" or self.new_operand:
-                self.result.value = data
-                self.new_operand = False
+            # Evitar ceros a la izquierda
+            if self.expression == "" or self.expression == "0":
+                if data == ".":
+                    self.expression = "0."
+                else:
+                    self.expression = data
             else:
-                self.result.value = self.result.value + data
+                self.expression += data
+                
+            self.result.value = self.expression
+            self.update_preview()
 
         elif data in ("+", "-", "*", "/"):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
-            self.operator = data
-            if self.result.value == "Error":
-                self.operand1 = "0"
+            if self.expression == "":
+                self.expression = "0" + data
+            # Si el último carácter ya es un operador, lo reemplazamos
+            elif self.expression[-1] in ("+", "-", "*", "/"):
+                self.expression = self.expression[:-1] + data
             else:
-                self.operand1 = float(self.result.value)
-            self.new_operand = True
+                self.expression += data
+                
+            self.result.value = self.expression
+            self.preview.value = ""
 
-        elif data in ("="):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
-            self.reset()
+        elif data == "=":
+            try:
+                res = eval(self.expression)
+                res_formatted = self.format_number(res)
+                self.formula.value = self.expression + " ="
+                self.result.value = str(res_formatted)
+                self.preview.value = ""
+                self.expression = str(res_formatted)
+            except Exception:
+                self.result.value = "Error"
+                self.expression = ""
 
-        elif data in ("%"):
-            self.result.value = float(self.result.value) / 100
-            self.reset()
+        elif data == "%":
+            try:
+                res = eval(self.expression) / 100
+                res_formatted = self.format_number(res)
+                self.result.value = str(res_formatted)
+                self.expression = str(res_formatted)
+                self.update_preview()
+            except Exception:
+                pass
 
-        elif data in ("+/-"):
-            if float(self.result.value) > 0:
-                self.result.value = "-" + str(self.result.value)
-
-            elif float(self.result.value) < 0:
-                self.result.value = str(
-                    self.format_number(abs(float(self.result.value)))
-                )
+        elif data == "+/-":
+            try:
+                res = eval(self.expression) * -1
+                res_formatted = self.format_number(res)
+                self.result.value = str(res_formatted)
+                self.expression = str(res_formatted)
+                self.update_preview()
+            except Exception:
+                pass
 
         self.update()
 
-    def format_number(self, num):
-        if num % 1 == 0:
-            return int(num)
-        else:
-            return num
-
-    def calculate(self, operand1, operand2, operator):
-        if operator == "+":
-            return self.format_number(operand1 + operand2)
-
-        elif operator == "-":
-            return self.format_number(operand1 - operand2)
-
-        elif operator == "*":
-            return self.format_number(operand1 * operand2)
-
-        elif operator == "/":
-            if operand2 == 0:
-                return "Error"
+    def update_preview(self):
+        try:
+            # Solo evalúa si hay operadores en la cadena y no termina en uno
+            if any(op in self.expression for op in ("+", "-", "*", "/")) and self.expression[-1] not in ("+", "-", "*", "/"):
+                res = eval(self.expression)
+                self.preview.value = f"= {self.format_number(res)}"
             else:
-                return self.format_number(operand1 / operand2)
+                self.preview.value = ""
+        except Exception:
+            self.preview.value = ""
 
-    def reset(self):
-        self.operator = "+"
-        self.operand1 = 0
-        self.new_operand = True
-
+    def format_number(self, num):
+        try:
+            num = float(num)
+            if num % 1 == 0:
+                return int(num)
+            return round(num, 4)
+        except:
+            return num
 
 def main(page: ft.Page):
     page.title = "Calc App"
-    # create application instance
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    
     calc = CalculatorApp()
-
-    # add application's root control to the page
     page.add(calc)
-
 
 ft.run(main)
